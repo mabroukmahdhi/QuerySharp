5460af282d2c916614969d9327d59e26cae0d0f6
Mabrouk Mahdhi <coding@mahdhi.com>
2025-01-27 23:30:57 +0100

    FOUNDATIONS: TranslateExpression

---
+++ QuerySharp/Services/Foundations/Expressions/IExpressionService.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi 2024. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Linq.Expressions;
+
+namespace QuerySharp.Services.Foundations.Expressions
+{
+    internal interface IExpressionService
+    {
+        string TranslateExpression(Expression expression);
+    }
+}
