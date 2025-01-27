4dd42279195bd44c8394c4c80fec6190a0777b41
Mabrouk Mahdhi <coding@mahdhi.com>
2025-01-28 00:07:34 +0100

    EXPOSERS: Query Builder

--- QuerySharp/Services/Expressions/ExpressionService.cs
+++ QuerySharp/Services/Expressions/ExpressionService.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace QuerySharp.Services.Foundations.Expressions
+namespace QuerySharp.Services.Expressions
 {
     internal class ExpressionService : ExpressionVisitor, IExpressionService
     {
--- QuerySharp/Services/Expressions/IExpressionService.cs
+++ QuerySharp/Services/Expressions/IExpressionService.cs
@@ -4,7 +4,7 @@
 
 using System.Linq.Expressions;
 
-namespace QuerySharp.Services.Foundations.Expressions
+namespace QuerySharp.Services.Expressions
 {
     internal interface IExpressionService
     {
